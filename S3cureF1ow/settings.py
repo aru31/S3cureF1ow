5f929ede9d52f5598fad986a84aa16bd132fed17
Arpit Gupta <guptarpit1997@gmail.com>
2018-09-30 01:28:03 +0530

    Made Secure wallet for user

--- S3cureF1ow/settings.py
+++ S3cureF1ow/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from user.models import Identity
+from django.views import generic
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'user/wallet.html'
+    context_object_name = 'identity'
+
+    def get_queryset(self):
+        return Identity.objects.all()
